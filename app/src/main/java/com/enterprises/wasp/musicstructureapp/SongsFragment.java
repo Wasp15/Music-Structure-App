f1f69a892262711037e125428dde062d582370a4
Wasp15 <estian1231@gmail.com>
2018-02-07 11:37:40 +0200

    Fixed bugs, added viewpager selector class.

--- app/src/main/java/com/enterprises/wasp/musicstructureapp/SongsFragment.java
+++ app/src/main/java/com/enterprises/wasp/musicstructureapp/SongsFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,9 +18,10 @@ public class SongsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.universal_container, container, false);
+        super.onCreate(savedInstanceState);
+
+        return rootView;
     }
 }
 
