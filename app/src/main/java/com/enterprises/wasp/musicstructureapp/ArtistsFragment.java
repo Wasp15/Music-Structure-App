eef07d8cdf4f9f810cbb5734cc457f26159471b9
Wasp15 <estian1231@gmail.com>
2018-02-08 10:00:59 +0200

    Added the ArtistFragment.

--- app/src/main/java/com/enterprises/wasp/musicstructureapp/ArtistsFragment.java
+++ app/src/main/java/com/enterprises/wasp/musicstructureapp/ArtistsFragment.java
@@ -1,17 +1,25 @@
 package com.enterprises.wasp.musicstructureapp;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ArtistsFragment extends Fragment {
 
+    private ArrayList<String> artistNames = new ArrayList<>();
 
     public ArtistsFragment() {
         // Required empty public constructor
@@ -20,9 +28,58 @@ public class ArtistsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.universal_layout, container, false);
+        super.onCreate(savedInstanceState);
+        ListView listView;
+        InfoAdapter adapter;
+        scanDeviceForMp3Files();
+        Collections.sort(artistNames);
+
+        ArrayList<Info> iSongs = new ArrayList<>();
+
+        listView = rootView.findViewById(R.id.songList);
+        for (int i = 0; i < artistNames.size(); i++) {
+            iSongs.add(new Info(artistNames.get(i)));
+        }
+
+        adapter = new InfoAdapter(getActivity(), iSongs);
+        listView.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    private void scanDeviceForMp3Files() {
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+        String[] projection = {
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.DATA,
+        };
+        final String sortOrder = MediaStore.Audio.AudioColumns.TITLE + " COLLATE LOCALIZED ASC";
+
+        Cursor cursor = null;
+        try {
+            Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+            cursor = getActivity().getContentResolver().query
+                    (uri, projection, selection, null, sortOrder);
+            if (cursor != null) {
+                cursor.moveToFirst();
+
+                while (!cursor.isAfterLast()) {
+                    String artist = cursor.getString(0);
+                    String path = cursor.getString(1);
+                    cursor.moveToNext();
+                    if (path != null && !artistNames.contains(artist)) {
+                        artistNames.add(artist);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e("TAG", e.toString());
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
     }
 }
 
